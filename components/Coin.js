d27b8287f1ce6e15cdf6abe75f9eae554b62faf7
Devin Parkinson <dparky13@icloud.com>
2018-04-04 22:46:51 -0600

    working

--- components/Coin.js
+++ components/Coin.js
@@ -1,33 +1,54 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { ListItem } from 'react-native-elements';
+import { StyleSheet, Text } from 'react-native';
+import {
+  Card,
+  ListItem,
+} from 'react-native-elements'
+import Swipeout from 'react-native-swipeout';
+import { removeCoin } from '../actions/coins';
+import { connect } from 'react-redux';
 
 class Coin extends React.Component {
-  state ={ color: 'green' }
+  state = { color: 'green' }
 
   componentWillReceiveProps(nextProps) {
     const { price } = nextProps;
-    let color;
-    if (price !== this.props.price) {
+    let color = this.state.color;
+    if ( price !== this.props.price ) {
       if (price > this.props.price)
         color = 'green'
       else
         color = 'red'
-
-      this.setState({ color })
     }
+    this.setState({ color })
+  }
+
+  swipeButtons = () => {
+    const { dispatch, id } = this.props;
+    return [{
+      text: 'Delete',
+      backgroundColor: 'red',
+      onPress: () => { dispatch(removeCoin(id)) }
+    }]
   }
 
   render() {
     const { symbol, price } = this.props;
     const { color } = this.state;
     return (
-      <ListItem
-        rightTitle={`$${parseFloat(price).toFixed(2)}`}
-        rightTitleStyle={styles[color]}
-        title={symbol}
-        titleStyle={styles.title}
-      />
+      <Swipeout
+        right={this.swipeButtons()}
+        autoClose={true}
+        backgroundColor="transparent"
+      >
+        <ListItem
+          rightTitle={`$${parseFloat(price).toFixed(2)}`}
+          rightTitleStyle={styles[color]}
+          title={symbol}
+          titleStyle={styles.title}
+          hideChevron={true}
+        />
+      </Swipeout>
     )
   }
 }
@@ -40,15 +61,14 @@ const styles = StyleSheet.create({
     fontSize: 20,
     width: 100,
     padding: 5,
+    flex: 1,
   },
   red: {
     backgroundColor: 'red',
     color: 'white',
     width: 100,
     fontSize: 20,
-    padding: 5,
   },
 });
 
-
-export default Coin;
+export default connect()(Coin);
